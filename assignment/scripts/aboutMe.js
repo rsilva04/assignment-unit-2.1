8acb3f91de861863e82b2f151188cdc3044d5fce
Rafael Silva <rafsilva004@gmail.com>
2023-06-14 15:34:31 -0500

    created conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -40,6 +40,12 @@ if(luckyNumber=='2'||adventurous==true)
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+console.log("I can have more pets!");
+  if(pets === allowedPets) {
+console.log("I have enough pets");
+} else {
+console.log("Oh no, I have too many pets!");
+}
 
 // STRETCH GOALS:
 
