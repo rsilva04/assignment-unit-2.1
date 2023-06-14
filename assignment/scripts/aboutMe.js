0cae10cdd8fbf82cf72c840cf11a4c13b2a2db2e
Rafael Silva <rafsilva004@gmail.com>
2023-06-14 15:11:21 -0500

    created contitional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -28,7 +28,8 @@ pets=pets +2;
 const allowedPets=5
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if(adventurous == true){console.log('Adventures are great!');}
+else{console.log('How about we stay home?');}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
