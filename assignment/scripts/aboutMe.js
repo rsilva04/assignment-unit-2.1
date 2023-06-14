f54ade913789d0a8c28883a58297cad18eb1bed0
Rafael Silva <rafsilva004@gmail.com>
2023-06-14 13:53:13 -0500

    added lastName variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -2,7 +2,7 @@
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 LetfirstName='Rafael';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+LetlastName='Silva';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 
